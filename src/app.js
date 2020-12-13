a9c31dae5f64664e13165b29a9872ac64f8be76a
makmakkerti <makmakkerti007@gmail.com>
2020-12-13 14:09:49 +0200

    test code removed

--- src/app.js
+++ src/app.js
@@ -1,29 +1,6 @@
-/* eslint-disable no-console */
-/* eslint-disable max-classes-per-file */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 // eslint-disable-next-line no-undef
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-class OldSyntax {
-  constructor() {
-    this.name = 'Mike';
-  }
-}
-
-const old = new OldSyntax();
-console.log(old);
-
-class NewSyntax {
-  name = 'Mak';
-
-  getGreeting = () => {
-    return `Hi! My name is ${this.name}`;
-  }
-}
-
-const newS = new NewSyntax();
-const greeting = newS.getGreeting();
-console.log(greeting);
